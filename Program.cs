eb493d1c82c013cbf9ddbdddffc377b36a011119
thetrung <thetrung.research@gmail.com>
2020-10-25 00:44:10 +0700

    Sketch a demo on the new language.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lang01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
